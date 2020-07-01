5cdfd30099afde5891e5abdaf17d31bd7a6cbe04
Jnanika <63325807+Jnanika@users.noreply.github.com>
2020-07-01 08:02:39 -0500

    Update TestBase.java

--- PizzaHTest/src/main/java/com/pizzhut/qa/base/TestBase.java
+++ PizzaHTest/src/main/java/com/pizzhut/qa/base/TestBase.java
@@ -1,70 +1,74 @@
-package com.pizzhut.qa.util;
+package com.pizzhut.qa.base;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
-//import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-//import org.testng.annotations.DataProvider;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
-import com.pizzhut.qa.base.TestBase;
+import com.pizzhut.qa.util.TestUtil;
+import com.pizzhut.qa.util.WebEventListener;
 
-public class TestUtil extends TestBase {
+public class TestBase {
 
-	public static long PLT = 40;
-	public static long IMP_wait = 60;
+	public static WebDriver driver;
+	public static Properties prop;
+	public static EventFiringWebDriver e_driver;
 
+	public static WebEventListener eventListener;
+//public static WebEventListener eventListener;
 
-	public static String TESTDATA_SHEET_PATH = "C:\\Users\\manju\\git\\PizzaHFinal\\PizzaHTest\\src\\main\\java\\com\\pizzahut\\qa\\testdata\\PizzaHutTestData.xlsx";
-
-
-	static Workbook book;
-	static Sheet sheet;
-	static JavascriptExecutor js;
-
-	public static Object[][] getTestData(String sheetName) {
-		FileInputStream file = null;
+	public TestBase() {
 		try {
-			file = new FileInputStream(TESTDATA_SHEET_PATH);
+			prop = new Properties();
+
+			FileInputStream ip = new FileInputStream(
+					"C:\\Users\\manju\\git\\PizzaHFinal\\PizzaHTest\\src\\main\\java\\com\\pizzahut\\qa\\configuration\\config.properties");
+
+			prop.load(ip);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
-		try {
-			book = WorkbookFactory.create(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		sheet = book.getSheet(sheetName);
-		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
-		// System.out.println(sheet.getLastRowNum() + "--------" +
-		// sheet.getRow(0).getLastCellNum());
-		for (int i = 0; i < sheet.getLastRowNum(); i++) {
-			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
-				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
-				//System.out.println(data[i][k]);
-			}
-		}
-		return data;
 	}
-	
-	public static void takeScreenshotAtEndOfTest() throws IOException {
-		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		String currentDir = System.getProperty("user.dir");
-		FileUtils.copyFile(scrFile, new File(currentDir + "\\screenshots\\" + System.currentTimeMillis() + ".png"));
+
+	public static void initialization() {
+		String browserName = prop.getProperty("browser");
+
+		if (browserName.equals("chrome")) {
+
+			// System.setProperty("webdriver.chrome.driver",
+			// "C:\\driver2\\chromedriver.exe");
+
+			System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+
+			driver = new ChromeDriver();
+
+		} else if (browserName.equals("FF")) {
+			System.setProperty("webdriver.gecko.driver", "C:\\QA\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+
+		e_driver = new EventFiringWebDriver(driver);
+		// Now create object of EventListerHandler to register it with
+		// EventFiringWebDriver
+		eventListener = new WebEventListener();
+		e_driver.register(eventListener);
+		driver = e_driver;
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PLT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMP_wait, TimeUnit.SECONDS);
+
+		driver.get(prop.getProperty("url"));
+
 	}
 
 }
-
-
-
-
-
-
